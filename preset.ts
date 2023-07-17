73ba5bb4c2d03a37a2e0bb044c72794ec98c2a1e
Shaurya <shauryakumar002@gmail.com>
2023-07-18 08:27:13 +1000

    feat(database): update env to use sqlite driver and make empty database.sqlite

--- preset.ts
+++ preset.ts
@@ -6,5 +6,22 @@ export default definePreset({
   handler: async () => {
     await applyNestedPreset({ preset: "hybridly/preset" });
     await extractTemplates();
+
+    await editFiles({
+      title: "change db driver to sqlite",
+      files: ".env",
+      operations: [
+        {
+          type: "update-content",
+          update: (content) =>
+            content.replace("DB_CONNECTION=mysql", "DB_CONNECTION=sqlite"),
+        },
+        {
+          type: "remove-line",
+          match: /DB_HOST/,
+          count: 5,
+        },
+      ],
+    });
   },
 });
