6ee0332e8754209586f97c92c78f031f34decc11
Shaurya <shauryakumar002@gmail.com>
2023-07-18 13:06:03 +1000

    feat(ui): override tailwind config

---
+++ templates/base/tailwind.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from 'tailwindcss';
+
+export default {
+    content: ['./resources/**/*.{js,ts,vue,blade.php}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('./resources/application/ui.plugin'),
+    ],
+} satisfies Config;
