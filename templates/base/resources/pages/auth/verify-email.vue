17360a1ef9e833589a4609f7b9c1a2124db51c89
Shaurya <shauryakumar002@gmail.com>
2023-07-18 11:06:04 +1000

    feat(auth): email verification page

---
+++ templates/base/resources/pages/auth/verify-email.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const form = useForm({
+    method: 'POST',
+    url: '/email/verification-notification',
+    fields: {},
+});
+
+const status = useProperty('status');
+</script>
+
+<template layout="auth">
+    <form @submit.prevent="form.submit" class="space-y-5">
+        <span v-if="status === 'verification-link-sent'"
+            >A new email verification link has been emailed to you!</span
+        >
+
+        <h1>Please verify your email by clicking the link sent to you.</h1>
+
+        <button
+            type="submit"
+            class="button w-full"
+            :class="{ 'loading-absolute': form.processing }"
+            :disabled="form.processing"
+        >
+            Resend confirmation email
+        </button>
+    </form>
+</template>
